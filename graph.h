7d589cbfceadf8d385660a0db260ef2887fa4bc1
Ali <rostamiev@gmail.com>
2018-08-02 14:40:09 +0200

    small changes

--- graph.h
+++ graph.h
@@ -40,13 +40,13 @@ class graph {
     Graph g;
     property_map<Graph, vertex_color_t>::type color;
 public:
-    graph(unsigned long num_vertices) : g(Graph(num_vertices)) { init(); }
+    graph(unsigned long num_vertices) : g(Graph(num_vertices)) { }
 
-    graph() { init(); }
+    graph() { }
 
     void add_edge(int i, int j, int weight) { boost::add_edge(i, j, weight, g); }
 
-    void init() {
+    void init_colors() {
         color = get(vertex_color, g);
         for_each_v([&](Ver v) { put_color_v(v, 0); });
     }
@@ -118,6 +118,7 @@ public:
     }
 
     std::tuple<int,std::vector<int>> greedy_color_order(std::vector<int> order, int max_color) {
+        init_colors();
         std::vector<unsigned int> forbiddenColors(num_v(), -1);
         for(int v : order) {
             forbiddenColors[0] = v;
@@ -142,30 +143,22 @@ public:
         return {unique_colors.size(), colors};
     }
 
-    std::tuple<int,std::vector<int>> greedy_color(int max_color) {
-        std::vector<unsigned int> forbiddenColors(num_v(), -1);
-        for_each_v([&](int v) {
-            forbiddenColors[0] = v;
-            for_each_n(v, [&](int n) {
-                int c = get_color_v(n);
-                if (c > 0)forbiddenColors[c] = v;
-            });
-            //Find first color which can be assigned to v
-            auto result = find_if(forbiddenColors.begin(), forbiddenColors.end(), [&](int i) {return i != v;});
-            auto res_color = distance(forbiddenColors.begin(),result);
-            if(res_color < max_color)
-                put_color_v(v, res_color);
-        });
-
-        std::vector<int> colors;
+    int num_colors_of_neighbors(int v) {
         std::set<int> unique_colors;
-        for_each_v([&](int v){
-            colors.push_back(get_color_v(v) - 1);
-            int the_color = get_color_v(v);
-            if(the_color != -1)
-                unique_colors.insert(the_color);
+        for_each_n(v, [&](int n){
+           int c = get_color_v(n);
+           unique_colors.insert(c);
         });
-        return {unique_colors.size(), colors};
+        return unique_colors.size();
+    }
+
+    std::tuple<int,std::vector<int>> greedy_color(int max_color) {
+        init_colors();
+        std::vector<int> order(num_v(), 0);
+        for_each_v([&](int v) {
+            order[v] = v;
+        });
+        return greedy_color_order(order, max_color);
     }
 
     std::vector<int> optimum_order() {
@@ -188,6 +181,66 @@ public:
             ret.push_back(get<0>(t));
         return ret;
     }
+
+    std::vector<int> largest_first_order() {
+        std::vector<std::tuple<int,int>> vertex_weight;
+        for_each_v([&](int v){
+            vertex_weight.push_back({v,degree(v,g)});
+        });
+
+        std::sort(begin(vertex_weight), end(vertex_weight), [](auto const &t1, auto const &t2) {
+            return get<1>(t1) > get<1>(t2); // or use a custom compare function
+        });
+
+        std::vector<int> ret;
+        for(std::tuple<int,int> t : vertex_weight)
+            ret.push_back(get<0>(t));
+        return ret;
+    }
+
+    std::tuple<int,std::vector<int>> saturation_degree_ordering_coloring(int max_color) {
+        init_colors();
+        V_iter vi, vi_end;
+        std::tie(vi, vi_end) = vertices(g);
+        std::list<int> vs (vi, vi_end);
+        std::vector<unsigned int> forbiddenColors(num_v(), -1);
+        while(!vs.empty()) {
+            int sat_v;
+            std::vector<std::tuple<int, int>> vertex_num_color_neighbors;
+            for(int v : vs) {
+                vertex_num_color_neighbors.push_back({v, num_colors_of_neighbors(v)});
+            }
+
+            std::tuple<int, int> vc =
+                    *std::max_element(std::begin(vertex_num_color_neighbors), std::end(vertex_num_color_neighbors), [&](std::tuple<int, int> vc1, std::tuple<int, int> vc2){
+                        return get<1>(vc1) < get<1>(vc2);
+                    });
+            sat_v = std::get<0>(vc);
+
+            forbiddenColors[0] = sat_v;
+            for_each_n(sat_v, [&](int n) {
+                int c = get_color_v(n);
+                if (c > 0)forbiddenColors[c] = sat_v;
+            });
+            //Find first color which can be assigned to v
+            auto result = find_if(forbiddenColors.begin(), forbiddenColors.end(), [&](int i) {return i != sat_v;});
+            auto res_color = distance(forbiddenColors.begin(),result);
+            if(res_color < max_color)
+                put_color_v(sat_v, res_color);
+
+            vs.remove(sat_v);
+        }
+        std::vector<int> colors;
+        std::set<int> unique_colors;
+        for_each_v([&](int v){
+            colors.push_back(get_color_v(v)-1);
+            int the_color = get_color_v(v);
+            if(the_color != -1)
+                unique_colors.insert(the_color);
+        });
+        return {unique_colors.size(), colors};
+    }
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include "graph.h"
 #include <boost/graph/sequential_vertex_coloring.hpp>
 #include <boost/numeric/ublas/vector.hpp>
+#include <fstream>
+#include <numeric>
 
 int
 compute_misses(int num_colors, const std::vector<int> &color_vec, boost::numeric::ublas::matrix<int> &m);
@@ -12,72 +14,97 @@ compute_misses(int num_colors, const std::vector<int> &color_vec, boost::numeric
 using std::cout;
 using std::endl;
 
-graph matrix2graph(const boost::numeric::ublas::matrix<int>& m, int index) {
+graph matrix2graph(const boost::numeric::ublas::matrix<int> &m, int index) {
     graph g(m.size1());
+    std::vector<std::tuple<int,int,int>> edges;
     for (int i = 0; i < m.size1(); i++) {
         for (int j = i + 1; j < m.size1(); j++) {
-            int cnt = 0;
+            int E_discovered = 0, E_missed = 0;
             for (int k = 0; k < m.size2(); k++) {
-                if (m(k, i) != 0 && m(k, j) != 0) {
-                    cnt++;
+                if (m(k, i) != 0 && m(k, j) == 0) {
+                    E_discovered++;
+                } else if (m(k, i) == 0 && m(k, j) != 0) {
+                    E_discovered++;
+                } else if (m(k, i) != 0 && m(k, j) != 0) {
+                    E_missed += 2;
                 }
             }
-            if (cnt > index) {
-                g.add_edge(i, j, cnt);
-            }
+            int weight = E_discovered - E_missed;
+//            if(E_missed != 0) edges.push_back({i,j,weight});
+//            if (weight > index) {
+//                g.add_edge(i, j, weight);
+//            }
+            if(weight < index) g.add_edge(i, j, weight);
         }
     }
+//    sort(begin(edges),end(edges), [&](std::tuple<int,int,int> t1, std::tuple<int,int,int> t2){
+//        return get<2>(t1) < get<2>(t2);
+//    });
+//
+//    for(int i=0;i < edges.size();i++) {
+//        auto [v1, v2, w] = edges[i];
+//        if(w>=0) g.add_edge(v1,v2,w);
+//    }
     return g;
 }
 
-int main() {
+int main(int argc, const char *argv[]) {
     using boost::numeric::ublas::matrix;
     using boost::numeric::ublas::matrix_column;
+    auto matrix_name = argv[1];
+    int min_possible_color = std::stoi(argv[2]);
+    int max_possible_color = std::stoi(argv[3]);
 //    matrix_market mm("/home/rostam/b1_ss.mtx");
-    matrix_market mm("/home/rostam/bcsstk01.mtx");
+    matrix_market mm(matrix_name);
 //    matrix_market mm("/home/rostam/nos3.mtx");
     matrix<int> m = mm.to_ublas_matrix();
-    int min_index = 0;
-    int max_index = 2;
+    int min_index = 16;
+    int max_index = 16;
 
+    std::ofstream out("result");
     for (int index = min_index; index <= max_index; index++) {
-        graph g1 = matrix2graph(m,index);
-        graph g2 = matrix2graph(m,index);
-
+        graph g = matrix2graph(m, index);
 //        typedef property_map<Graph, boost::vertex_index_t>::const_type vertex_index_map;
 //        boost::iterator_property_map<int *, vertex_index_map> color(&color_vec.front(), boost::get(boost::vertex_index, g));
 //        int num_colors = boost::sequential_vertex_coloring(g, color);
+        out << "threshold_for_edge_weights " << index << endl;
+        for (int color = min_possible_color; color < max_possible_color; color++) {
+            auto [num_colors_natural, color_vec_natural] = g.greedy_color(color);
+            int all_misses_natural = compute_misses(num_colors_natural, color_vec_natural, m);
 
-        auto t = g1.greedy_color(100);
-        auto ord = g2.optimum_order();
-        auto t2 = g2.greedy_color_order(ord, 100);
+            std::vector<int> ord = g.optimum_order();
+            auto [num_colors_newIdea, color_vec_newIdea] = g.greedy_color_order(ord, color);
+            int all_misses_newIdea = compute_misses(num_colors_newIdea, color_vec_newIdea, m);
 
-        int num_colors1 = std::get<0>(t);
-        std::vector<int> color_vec1 = std::get<1>(t);
+            std::vector<int> lfo_ord = g.largest_first_order();
+            auto [num_colors_lfo, color_vec_lfo] = g.greedy_color_order(lfo_ord, color);
+            int all_misses_lfo = compute_misses(num_colors_lfo, color_vec_lfo, m);
 
-        int num_colors2 = std::get<0>(t2);
-        std::vector<int> color_vec2 = std::get<1>(t2);
+            auto [num_colors_sat, color_vec_sat] = g.saturation_degree_ordering_coloring(color);
+            int all_misses_sat = compute_misses(num_colors_sat, color_vec_sat, m);
 
-        int all_sum1 = compute_misses(num_colors1, color_vec1, m);
-        int all_sum2 = compute_misses(num_colors2, color_vec2, m);
 
-//        //        cout << misses[0] << endl;
-        cout << color_vec1 << " "  << color_vec2 << endl;
-        cout << num_colors1 << " " << num_colors2 << endl;
-        cout << all_sum1 << " " << all_sum2 << endl << endl;
+            out << "max_color " << color << endl;
+            out << "num_colors (nat, new, lfo, sat): " << num_colors_natural << " " << num_colors_newIdea << " "
+                << num_colors_lfo << " " << num_colors_sat << endl;
+            out << "num_misses (nat, new, lfo, sat): " << all_misses_natural << " " << all_misses_newIdea << " "
+                << all_misses_lfo << " " << all_misses_sat << endl << endl;
+        }
     }
+    out.flush();
+    out.close();
     return 0;
 }
 
 int compute_misses(int num_colors, const std::vector<int> &color_vec, boost::numeric::ublas::matrix<int> &m) {
     int all_sum = 0;
-    std::vector<boost::numeric::ublas::vector<int>> misses(num_colors);
-    for (int i = 0; i < num_colors; i++) {
+    std::vector<boost::numeric::ublas::vector<int>> misses(num_colors + 1);
+    for (int i = 0; i < num_colors + 1; i++) {
         misses[i] = boost::numeric::ublas::zero_vector<int>(m.size2());
     }
-
     for (int i = 0; i < color_vec.size(); i++) {
-        if(color_vec[i] != -1) misses[color_vec[i]] += column(m, i);
+        if (color_vec[i] != -1) misses[color_vec[i]] += column(m, i);
+        else misses[num_colors] += column(m, i);
     }
     for (int i = 0; i < misses.size(); i++) {
         misses[i] /= 2;
